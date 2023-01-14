4710c91fee232211469184a012f1e6b1a544646f
Semanur12 <119580454+Semanur12@users.noreply.github.com>
2023-01-14 18:54:44 +0300

    XML File

---
+++ src/Gun05/_02_Intro.java
@@ -0,0 +1,17 @@
+package Gun05;
+
+import org.testng.annotations.Test;
+
+public class _02_Intro {
+
+
+    @Test
+    void Test1(){System.out.println("Test 1");}
+
+    @Test
+    void Test2(){System.out.println("Test 2");}
+
+
+
+
+}
